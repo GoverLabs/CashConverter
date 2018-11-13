a7873dfdaa0e5194f211ff6043fd7e2895223451
Nikita Smirnov <general.governator@gmail.com>
2018-11-13 17:15:54 +0200

    Fix review issues

--- app/src/main/java/currencyConverter/codes/CodeUtils.java
+++ app/src/main/java/currencyConverter/codes/CodeUtils.java
@@ -2,15 +2,20 @@ package currencyConverter.codes;
 
 import java.util.List;
 
-import currencyConverter.codes.CountryCode;
-import currencyConverter.codes.CurrencyCode;
+import currencyConverter.exception.CurrencyCodeFetchingException;
 
 public class CodeUtils {
 
-    public static CurrencyCode getCurrencyFromCountry(CountryCode countryCode) {
+    public static CurrencyCode getCurrencyFromCountry(CountryCode countryCode) throws CurrencyCodeFetchingException {
         List<com.neovisionaries.i18n.CurrencyCode> currencyCodeList =
             com.neovisionaries.i18n.CurrencyCode.getByCountryIgnoreCase( countryCode.toStringISO() );
 
-        return new CurrencyCode( currencyCodeList.get(0).getName() );
+		com.neovisionaries.i18n.CurrencyCode originalCode= currencyCodeList.get(0);
+
+		if(originalCode != null) {
+			return new CurrencyCode( originalCode.getName() );
+		} else {
+			throw new CurrencyCodeFetchingException();
+		}
     }
 }
