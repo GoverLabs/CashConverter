f43eb42adb765f103806182fb613ddcf18a80a71
GoverLabs <general.governator@gmail.com>
2018-11-26 22:20:46 +0200

    Fix review remarks

--- app/src/main/java/currencyConverter/codes/CodeUtils.java
+++ app/src/main/java/currencyConverter/codes/CodeUtils.java
@@ -22,30 +22,22 @@ public class CodeUtils {
 		}
     }
 
-    public static List<CurrencyCode> getAvailableCurrencyCodes(boolean sortByName) {
+    public static List<CurrencyCode> getAvailableCurrencyCodes() {
     	ArrayList<CurrencyCode> codeList = new ArrayList<CurrencyCode>();
 
 	    for (com.neovisionaries.i18n.CurrencyCode currency : com.neovisionaries.i18n.CurrencyCode.values()) {
 		    codeList.add(new CurrencyCode(currency));
 	    }
 
-//	    if(sortByName) {
-//		    Collections.sort(codeList);
-//	    }
-
 	    return codeList;
     }
 
-	public static List<CountryCode> getAvailableCountryCodes(boolean sortByName) {
+	public static List<CountryCode> getAvailableCountryCodes() {
 		ArrayList<CountryCode> codeList = new ArrayList<CountryCode>();
 
 		for (com.neovisionaries.i18n.CountryCode country : com.neovisionaries.i18n.CountryCode.values()) {
 			codeList.add(new CountryCode(country.getAlpha2()));
 		}
-
-//	    if(sortByName) {
-//		    Collections.sort(codeList);
-//	    }
 
 		return codeList;
 	}
